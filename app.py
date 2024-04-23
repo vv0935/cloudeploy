229cf49753c19ba72c4ad79e0fdaadf5e562e656
vv0935 <104317567+vv0935@users.noreply.github.com>
2024-04-23 18:53:47 +0000

    added home template jobitem

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+jobs=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
 
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'Rs. 12,00,000'
+  }
+]
 @app.route("/")
 def hello():
-  return "Hello Weeeeeorld"
+  return render_template('home.html',JOBS=jobs,company_name='Jovian')
 
 
 if __name__ == '__main__':
